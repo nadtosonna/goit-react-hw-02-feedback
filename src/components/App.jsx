cdad450d1d8b38b32c5b39d07fb402839211818e
nadtosonna <prasolova.kateryna@gmail.com>
2022-09-17 14:30:35 +0300

    feedback finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
+import Notification from "./Notification/Notification";
+import Section from "./Section/Section";
+import Statistics from "./Statistics/Statistics";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  onLeaveFeedback = name => {
+    this.setState(prevState => {
+      return {
+        [name]: prevState[name] + 1,
+      };
+    });
+  };
+
+    render() {
+      return (
+        <div>
+          <Section title="Please leave your feedback:">
+            <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback} />
+          </Section>
+          <Section title="Statistics:">
+            {this.state.good || this.state.bad || this.state.neutral > 0 ?
+              <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad} /> :
+              <Notification message="There is no feedback yet." />}
+
+            </Section>
+        </div>
+
+        )
+    }
+}
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import css from './Section.module.css';
+
+function Section({ title, children }) {
+    return (
+        <section className={css.section}>
+            {title && <h2>{title}</h2>} {children}
+        </section>
+    );
+}
+
+Section.propTypes = {
+    title: PropTypes.string,
+    children: PropTypes.node
+}
+
+export default Section;
